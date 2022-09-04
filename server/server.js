b280e24421705c3a6d12aaabc00675ac2d9dd4e3
Samy <samy.belarbi@3wa.io>
2022-09-04 15:19:50 +0200

    Added a Procfile

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express"); // Importer Express
 const cors = require("cors"); // Pour contourner le CORS POLICY
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 app.use(cors()); // CORS POLICY
